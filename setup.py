0a409c2b31e05a6acbcab72bef5c38b59a427cdf
Etienne Deprit <edeprit@trssllc.com>
2022-10-04 19:34:48 -0400

    Add setup and requirements for Python packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="saac",
+    version="0.0.1",
+    author="TRSS",
+    author_email="valeria.rozenbaum@trssllc.com",
+    description="Framework for text-to-image auditing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.trssllc.com/datascience/SAAC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.8',
+    install_requires=required
+)
