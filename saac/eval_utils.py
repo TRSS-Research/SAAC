bc8f48c0c421f9d1ca224a3c8e9d15129694d8ca
Spencer Torene <spencer.torene@trssllc.com>
2022-10-08 22:29:38 -0400

    Histograms, RGB intensity sorting, some stats.

--- saac/eval_utils.py
+++ saac/eval_utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import warnings
 
@@ -30,5 +31,13 @@ def load_tda_data():
     return tda_data
 
 
+# https://stackoverflow.com/questions/687261/converting-rgb-to-grayscale-intensity
+def rgb_intensity(rgb_tuple):
+    return np.mean(np.dot([0.2126, 0.7152, 0.0722], np.array(rgb_tuple)))
+
+
 def rgb_sorter(rgb_tuples):
-    return sorted(rgb_tuples, key=lambda x: sum(x), reverse=False)
+    # return sorted(rgb_tuples, key=lambda x: sum(x), reverse=False)
+    return sorted(rgb_tuples, key=lambda x: rgb_intensity(x), reverse=False)
+
+
