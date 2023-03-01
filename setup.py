37d23aeecf512d3019fa9d9b9d628d9163ce55c0
zseid <zseid@trssllc.com>
2023-03-01 14:31:44 -0500

    add interim files

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setuptools.setup(
         'saac':['./image_analysis/models/gender_model/gender_model_default_calibrated.joblib',
                 './prompt_generation/data/raw/TraitDescriptiveAdjectives.csv',
                 './prompt_generation/data/raw/OEWS21_OccupationsDetailedView.csv',
+                './prompt_generation/data/interim/AnnualOccupations_TitleBank.csv',
+                './prompt_generation/data/interim/TDA_Bank.csv',
                 './evaluation/data/raw/stable_diffusion_raw_processed.csv',
                 './evaluation/data/raw/midjourney_deepface_calibrated_equalized_mode.csv',
                 './prompt_generation/data/processed/generated_mj_prompts.csv'
